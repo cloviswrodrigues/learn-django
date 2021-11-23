8ce0025c2e4a9c836057cd481cdab959cdaac7a6
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2021-11-23 01:31:02 -0300

    implementando CRUD project

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
-    tag = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     vale_total = models.IntegerField(default=0, null=True, blank=True)
     vale_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
