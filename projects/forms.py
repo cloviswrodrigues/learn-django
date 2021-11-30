f7c3cc7ecf3ddda2b51d8c28d3b2de19cf35b815
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2021-11-30 00:58:16 -0300

    desenvolvendo

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from django import forms
 from .models import Project
 
@@ -9,3 +9,10 @@ class ProjectForm(ModelForm):
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+        
