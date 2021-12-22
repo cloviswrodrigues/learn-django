8214b21155ae6e574a3766657b972e23c3c4bc83
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2021-12-21 23:05:54 -0300

    implementado login e logout

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 
@@ -16,6 +17,7 @@ def project(request, pk):
     context = { 'project': project }
     return render(request, 'projects/single-project.html', context)   
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
 
@@ -28,6 +30,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -41,6 +44,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)    
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
