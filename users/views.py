cde2ee140ae4105ec1b74b46d836fa4acec16154
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2022-01-06 00:23:14 -0300

    adicionado funcionalidade de editar e deletar skills

--- users/views.py
+++ users/views.py
@@ -109,7 +109,35 @@ def createSkill(request):
             skill = form.save(commit=False)
             skill.owner = profile
             skill.save()
+            messages.success(request, 'Skill was added successfully!')
             return redirect('account')
 
     context = { 'form': form }
     return render(request,'users/skill_form.html', context)
+
+def updateSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+
+    if request.method == 'POST':
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Skill was updated successfully!')
+            return redirect('account')
+
+    context = { 'form': form }
+    return render(request,'users/skill_form.html', context)    
+
+def deleteSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+
+    if request.method == 'POST':
+        skill.delete()
+        messages.success(request, 'Skill was deleted successfully!')
+        return redirect('account')
+
+    context = {'object': skill}
+    return render(request, 'delete_template.html', context)
