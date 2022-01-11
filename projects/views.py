0f10e4b4f4ee4242d6ceb46759963f597a329031
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2022-01-10 23:27:16 -0300

    implementando paginação na view da projects

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Project
 from .forms import ProjectForm
 from .utils import searchProject
@@ -10,8 +11,21 @@ from .utils import searchProject
 def projects(request):
     
     projects, search_query = searchProject(request)
+
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
     
-    context = { 'projects': projects, 'search_query': search_query }
+    context = { 'projects': projects, 'search_query': search_query, 'paginator': paginator }
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
