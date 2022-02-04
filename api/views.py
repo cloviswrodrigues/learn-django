934c107c2e02f7d678c853a2ab674c42467a75d0
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2022-02-03 23:54:25 -0300

    criando API

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refreshs'},
+    ]
+
+    return Response(routes)
